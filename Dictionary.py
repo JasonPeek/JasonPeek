9ccb104b7ffeb160b8ec0f41051b2250daedb6bc
Jason Peek <jason.peek@stu.jefferson.kyschools.us>
2018-03-19 09:30:46 -0400

    second commit

---
+++ Dictionary.py
@@ -0,0 +1,10 @@
+math_terms = {
+    "calculus": "a branch of mathematics involving derivatives and integrals, used to study motion and changing values", 
+    "prime numbers": "integers that are greater than one and divisible by themselves and one",
+    "right triangle": "a triangle with an angle of 90 degrees",
+    "slope": "the steepness or incline of a line, determined by reference to two points on the line, e.g",
+    "set": "a collection of distinct objects or numbers, without regard to their order, considered as an object in its own right",
+    "fraction": " a way of writing rational numbers used to represent ratios or division"
+    }
+for k,v in math_terms.items():
+    print (k,v)
