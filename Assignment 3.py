d48902ea1c5bc77257992f93488968640f87ce28
Jason Peek <jason.peek@stu.jefferson.kyschools.us>
2018-03-26 09:50:04 -0400

    third commit

---
+++ Assignment 3.py
@@ -0,0 +1,14 @@
+grade_list = []
+
+math_grade = int(input("what is your math grade?: "))
+science_grade = int(input("what is your science grade?: "))
+english_grade = int(input("what is your english grade?: "))
+computer_grade = int(input("what is your computer grade?: "))
+extra_grade = int(input("what is your extra class grade?: "))
+
+grades = math_grade, science_grade, english_grade, computer_grade, extra_grade
+
+average = (sum)(grades)/5
+print (average)
+grade_list.append(grades)
+print (grade_list)
